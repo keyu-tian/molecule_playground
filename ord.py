c9767d8a9f63a21390e8b78df3090f878cb0a531
tiankeyu <tiankeyu@bytedance.com>
2021-12-07 12:59:52 +0800

    [upd] bad reaction

--- ord.py
+++ ord.py
@@ -3,14 +3,19 @@ import json
 import os
 import pathlib
 import time
-from typing import List, Dict
+import zipfile
+from typing import List, Dict, Tuple
 
 import torch
 import tqdm
+import wget
+
+# install ord_schema following https://github.com/open-reaction-database/ord-schema
 from ord_schema import message_helpers
 from ord_schema.proto import dataset_pb2
-from rdkit import RDLogger
 
+# conda install -c rdkit rdkit
+from rdkit import RDLogger
 RDLogger.DisableLog('rdApp.*')
 
 import smiles2graph
@@ -20,8 +25,20 @@ def time_str():
     return datetime.datetime.now().strftime('[%m-%d %H:%M:%S]')
 
 
-def jsonfy_ord():
-    dataset_root = pathlib.Path(os.path.expanduser('~')) / 'datasets' / 'ord-data-zip' / 'data'
+def download_and_jsonfy_data():
+    home_path = pathlib.Path(os.path.expanduser('~'))
+    dataset_root = home_path / 'datasets' / 'ord-data-zip' / 'data'
+    if not os.path.exists(dataset_root):
+        datasets_dir = str(home_path / 'datasets')
+        zip_file = wget.download(url='https://github.com/open-reaction-database/ord-data/archive/refs/heads/main.zip', out=datasets_dir)
+        fz = zipfile.ZipFile(zip_file, 'r')
+        for file in fz.namelist():
+            zip_dirname = os.path.split(file)[0]
+            fz.extract(file, datasets_dir)
+        fz.close()
+        os.rename(os.path.join(datasets_dir, zip_dirname), os.path.join(datasets_dir, 'ord-data-zip'))
+        assert os.path.exists(dataset_root)
+        
     all_gz_paths = []
     for dir_name in sorted(os.listdir(dataset_root)):
         data_root = dataset_root / dir_name
@@ -29,7 +46,7 @@ def jsonfy_ord():
         for i, gz_name in enumerate(data):
             all_gz_paths.append((f'{dir_name}--{i + 1}of{len(data)}', str(data_root / gz_name)))
     
-    output_dir = pathlib.Path(os.path.expanduser('~')) / 'datasets' / 'ord-data-json'
+    output_dir = home_path / 'datasets' / 'ord-data-json'
     os.makedirs(output_dir, exist_ok=True)
     gz_len = len(all_gz_paths)
     for i, (ds_name, gz_path) in enumerate(all_gz_paths):
@@ -56,7 +73,7 @@ def jsonfy_ord():
         print(f'{time_str()} {ds_desc} dumped @ {output_file}, load={load_t-start_t:.2f} cast={cast_t-start_t:.2f}, dump={dump_t-cast_t:.2f}')
 
 
-def main():
+def tensorfy_json_data():
     jsons_root = pathlib.Path(os.path.expanduser('~')) / 'datasets' / 'ord-data-json'
     torch_root = pathlib.Path(os.path.expanduser('~')) / 'datasets' / 'ord-data-torch'
     os.makedirs(torch_root, exist_ok=True)
@@ -175,9 +192,7 @@ def check_and_save(torch_file, json_name, bad_reaction_ids, reaction_ids, all_mo
 
 
 def reaction2graphs(bar, role2idx, one_reaction, mole_offset, edge_offset, atom_offset):
-    react_mole_roles = []
-    react_edge_index, react_edge_feat, react_atom_feat = [], [], []
-    react_edge_offset, react_atom_offset = [], []
+    role_smiles_pairs: List[Tuple[str, List[str]]] = []
     for k, v in one_reaction.items():
         if k.startswith('inputs') and k.endswith('.type') and v.upper() == 'SMILES':
             role = one_reaction.get(k.split('identifiers[')[0] + 'reaction_role', 'REACTANT').upper()
@@ -187,22 +202,46 @@ def reaction2graphs(bar, role2idx, one_reaction, mole_offset, edge_offset, atom_
             role = None
         if role is not None:
             for smiles in one_reaction[k.replace('.type', '.value')].split('.'):
-                react_mole_roles.append(role2idx[role])
-                mole_offset += 1
-                
-                #  (E, 2)     (E, 3)     (V, 9)
-                edge_index, edge_feat, atom_feat = smiles2graph.smiles2graph(smiles)
-                react_edge_index.append(edge_index), react_edge_feat.append(edge_feat), react_atom_feat.append(atom_feat)
-                
-                E, V = edge_index.shape[0], atom_feat.shape[0]
-                bar.set_postfix_str(f'role={role:9s}, E={E:3d}, V={V:3d}', refresh=False)
-                edge_offset += E
-                react_edge_offset.append(edge_offset)
-                atom_offset += V
-                react_atom_offset.append(atom_offset)
+                role_smiles_pairs.append((role, smiles))
+
+    if len(role_smiles_pairs) == 0:
+        for k, v in one_reaction.items():
+            if v.upper() == 'REACTION_SMILES':
+                left, mid, right = one_reaction[k.replace('.type', '.value')].split('>')
+                for reactant in left.split('.'):
+                    role_smiles_pairs.append(('REACTANT', reactant))
+                for solvent_or_catalyst in mid.split('.'):
+                    if any(x in solvent_or_catalyst for x in {'Pd', 'Pt', 'Fe', 'Au', 'Mn', 'Ni', 'Cu', 'Ag'}):
+                        role_smiles_pairs.append(('CATALYST', solvent_or_catalyst))
+                    else:
+                        role_smiles_pairs.append(('SOLVENT', solvent_or_catalyst))
+                for outcome in right.split('.'):
+                    role_smiles_pairs.append(('OUTCOME', outcome))
+                    
+    if len(role_smiles_pairs) == 0:
+        raise AttributeError('bad reaction')
+
+    react_mole_roles = []
+    react_edge_index, react_edge_feat, react_atom_feat = [], [], []
+    react_edge_offset, react_atom_offset = [], []
+    for role, smiles in role_smiles_pairs:
+        react_mole_roles.append(role2idx[role])
+        mole_offset += 1
+        
+        #  (E, 2)     (E, 3)     (V, 9)
+        edge_index, edge_feat, atom_feat = smiles2graph.smiles2graph(smiles)
+        react_edge_index.append(edge_index), react_edge_feat.append(edge_feat), react_atom_feat.append(atom_feat)
+        
+        E, V = edge_index.shape[0], atom_feat.shape[0]
+        bar.set_postfix_str(f'role={role:9s}, E={E:3d}, V={V:3d}', refresh=False)
+        edge_offset += E
+        react_edge_offset.append(edge_offset)
+        atom_offset += V
+        react_atom_offset.append(atom_offset)
                 
     return mole_offset, edge_offset, atom_offset, react_mole_roles, react_edge_index, react_edge_feat, react_atom_feat, react_edge_offset, react_atom_offset
 
 
 if __name__ == '__main__':
-    main()
+    # download_and_jsonfy_data()
+    tensorfy_json_data()
